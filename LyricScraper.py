1b14fc46fad0505879b0b4391d988a4bfaca75ca
Marc J <jutras.marc@gmail.com>
2016-01-31 19:10:56 -0500

    Revert "Small syntax corrections". Some corrections are invalid python3 code. Added Python 2 support to LyricScrape This reverts commit c88459bb802a1f0eb0caf49693b9b528bea783b4.

--- LyricScraper.py
+++ LyricScraper.py
@@ -1,30 +1,36 @@
-#! /usr/bin/python3
-''' Scrapes the lyrics from the eurobeat-prime.com database.
-Must have installed urllib.request?
-'''
+#!/usr/bin/env python3
+''' Scrapes the lyrics from the eurobeat-prime.com database.'''
 
-from SongData import SongData
-from bs4 import BeautifulSoup
-import urllib.request as request
+from __future__ import print_function
 import sys
 import multiprocessing
+
+if sys.version_info > (3, 0): # Using Python 3
+    import urllib.request as request
+else: # Using Python 2
+    import urllib2 as request
+
+
+
+from bs4 import BeautifulSoup
+from SongData import SongData
 
 def get_song_urls():
     ''' Reads the urls for all of the songs in the database.'''
     SONG_URL = 'http://www.eurobeat-prime.com/lyrics.php'
     ARTIST_URL = 'http://www.eurobeat-prime.com/lyrics.php?artist='
     ARTIST_OPTIONS = '1abcdefghijklmnopqrstuvwxyz'
-
+    
     song_urls = []
     for option in ARTIST_OPTIONS:
         artist_page = request.urlopen(ARTIST_URL + option)
-        soup = BeautifulSoup(artist_page.readall(), 'html.parser')
-
+        soup = BeautifulSoup(''.join(artist_page.readlines()), 'html.parser')
+        
         song_links = soup.find('div', class_='mmids').find_all("a")
         song_urls += [SONG_URL + link.get("href") for link in song_links]
-
+        
     return song_urls
-
+      
 def parallel_scrape_songs(urllist):
     ''' Scrapes the lyrics of a each song in a list of urls. Uses parallel threads.'''
     pool = multiprocessing.Pool(8)
@@ -40,7 +46,7 @@ def parallel_scrape_songs(urllist):
             short_title = data.title[:title_length] + ".." if \
                     len(data.title) > title_length else data.title
             out_string = short_artist + " - " + short_title
-            print "\r{:8.2f}% {:69s}".format(max_i/count*100, out_string)
+            print("\r{:8.2f}% {:69s}".format(max_i/count*100, out_string), end='')
         scrape_data.append(data)
     return scrape_data
 
@@ -52,12 +58,12 @@ def scrape_songs(urllist):
         result = scrape_song(url)
         scrape_data.append(result)
     return scrape_data
-
+    
 def scrape_song(url):
     ''' Scrapes the lyrics of a single song.'''
     song_page = request.urlopen(url)
-    song_soup = BeautifulSoup(song_page.readall(), 'html.parser')
-
+    song_soup = BeautifulSoup(''.join(song_page.readlines()), 'html.parser')
+    
     lyrics_box = song_soup.find('div', class_='mmids')
     artist_title = lyrics_box.b.extract().text
     artist = artist_title.split('-')[0].strip()
@@ -75,9 +81,9 @@ if __name__ == "__main__":
     lyric_dump = open('lyrics.txt',encoding='utf-8', mode='w')
     lyric_dump.writelines("\n".join([data.lyrics for data in results]))
     lyric_dump.close()
-
+    
     import pickle
     pickle_file = open('pickled.bin','wb')
     pickle.dump(results, pickle_file)
     pickle_file.close()
-
+    
